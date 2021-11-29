dd4fba806aa9e91acf8ed44bb450fe1cc11211dc
Andrey Tamelo <atamelo@servicetitan.com>
2021-11-29 12:15:35 -0800

    api brush-up

--- OutboxSample/Application/IOutbox.cs
+++ OutboxSample/Application/IOutbox.cs
@@ -1,8 +1,29 @@
-﻿using OutboxSample.Model;
-
-namespace OutboxSample.Application;
+﻿namespace OutboxSample.Application;
 
 public interface IOutbox : ISupportUnitOfWork
 {
-    void Publish<TPayload>(EventEnvelope<TPayload> @event);
+    void Send<TPayload>(EventEnvelope<TPayload> @event);
+
+    public record EventEnvelope<TPayload>
+    {
+        // TODO: add aggregate type, event type, event id
+
+        public Guid AggregateId { get; }
+
+        public TPayload Payload { get; }
+
+        public DateTime Timestamp { get; }
+
+        public int Version { get; }
+
+        public EventEnvelope(Guid aggregateId, TPayload payload, DateTime timestamp, int version)
+        {
+            ArgumentNullException.ThrowIfNull(payload, nameof(payload));
+
+            AggregateId = aggregateId;
+            Payload = payload;
+            Timestamp = timestamp;
+            Version = version;
+        }
+    }
 }
