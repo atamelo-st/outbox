3da4868cd936657de9f2ba0a45941118b56b416f
Andrey Tamelo <atamelo@servicetitan.com>
2021-12-07 08:04:19 -0800

    removed pub-sub binging; kept input binding

--- DaprListener/Controllers/WeatherForecastController.cs
+++ DaprListener/Controllers/WeatherForecastController.cs
@@ -1,4 +1,8 @@
+using Dapr;
 using Microsoft.AspNetCore.Mvc;
+
+// USE:
+// dapr run --app-id dapr-listener --app-port 5076 --dapr-http-port 3602 --dapr-grpc-port 60002 --components-path ../components -- dotnet run
 
 namespace DaprListener.Controllers
 {
@@ -16,8 +20,30 @@ namespace DaprListener.Controllers
         [HttpPost("/user-added")]
         public object Notify(object userEvent)
         {
-            this._logger.LogInformation("{userEvent}", userEvent);
+            this._logger.LogInformation("Input binding: {userEvent}", userEvent);
+            
+            
             return Ok(userEvent);
         }
+
+        public /*readonly*/ record struct UserAddedEvent
+        {
+            public Guid UserId { get; set; }
+            public string UserName { get; set; }
+
+            //public UserAddedEvent(Guid userId, string userName)
+            //{
+            //    ArgumentNullException.ThrowIfNull(userName, nameof(userName));
+
+            //    UserId = userId;
+            //    UserName = userName;
+            //}
+
+            //public UserAddedEvent()
+            //{
+            //    throw new NotSupportedException("Must use constructor with parameters.");
+            //}
+        }
+
     }
 }
--- DaprListener/Program.cs
+++ DaprListener/Program.cs
@@ -1,10 +1,14 @@
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder
     .Services
-        .AddControllers()
+        .AddControllers(options => 
+            options.InputFormatters.Insert(0, new TextPlainInputFormatter()))
         .AddDapr();
 
 var app = builder.Build();
@@ -20,3 +24,41 @@ app.MapControllers();
 app.MapSubscribeHandler();
 
 app.Run();
+
+public class TextPlainInputFormatter : TextInputFormatter
+{
+    private const string ContentType = "text/plain";
+
+    public TextPlainInputFormatter()
+    {
+        SupportedMediaTypes.Add(ContentType);
+    }
+
+    //public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+    //{
+    //    var request = context.HttpContext.Request;
+    //    using (var reader = new StreamReader(request.Body))
+    //    {
+    //        var content = await reader.ReadToEndAsync();
+    //        return await InputFormatterResult.SuccessAsync(content);
+    //    }
+    //}
+
+    public override bool CanRead(InputFormatterContext context)
+    {
+        var contentType = context.HttpContext.Request.ContentType;
+        return contentType?.StartsWith(ContentType) ?? false;
+    }
+
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(
+        InputFormatterContext context, 
+        Encoding encoding)
+    {
+        var request = context.HttpContext.Request;
+        using (var reader = new StreamReader(request.Body, encoding))
+        {
+            var content = await reader.ReadToEndAsync();
+            return await InputFormatterResult.SuccessAsync(content);
+        }
+    }
+}
